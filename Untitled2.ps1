fd5ae4be3f2da1b6d8b48d7119bef66e6c14bc5e
unknown <racker@L43VGKV1.RACKSPACE.CORP>
2014-11-07 11:09:07 -0600

    stuff

--- Untitled2.ps1
+++ Untitled2.ps1
@@ -41,17 +41,22 @@ $CSURI = (($catalog.access.serviceCatalog | ? name -like "CloudServersO*").endpo
 $CSDURI = (($catalog.access.serviceCatalog | ? name -like "CloudServersO*").endpoints | where region -like 'DFW').publicURL + '/servers/detail'
 $IMURI = (($catalog.access.serviceCatalog | ? name -like "CloudServersO*").endpoints | where region -like 'DFW').publicURL + '/images'
 (irm -Uri $IMURI -Headers $authToken -method get).images | ? name -like "*Windows*"
-$body = @{server = @{name = 'mswtest'; flavorRef = 'performance1-4'; imageRef = '25f64fd5-4d61-4d4a-8cdb-801de7d9d99b'} }
+$body = @{server = @{name = 'mswtest'; flavorRef = 'performance1-4'; imageRef = '25f64fd5-4d61-4d4a-8cdb-801de7d9d99b'} } | convertto-json -depth 30
 $newserver = irm -Uri $CSURI -Headers $authToken -method POST -Body $body -ContentType application/json
 
 #Building
 
-$building = irm -Uri $CSDURI -Headers $authToken -method get 
+$building = (irm -Uri $CSDURI -Headers $authToken -method get).servers | ? name -like '*mswtest*' 
 while ($building.servers.status -ne 'ACTIVE' )
     {
-
+    write-host Building Server
+    sleep 20
+    $building = (irm -Uri $CSDURI -Headers $authToken -method get).servers | ? name -like '*mswtest*' 
     }
-
+write-host $newserver.server.name
+writehost $newserver.server.id
+write-host $newserver.server.adminpass
+writehost $building.ipv4address 
 
 #Create LB
 
